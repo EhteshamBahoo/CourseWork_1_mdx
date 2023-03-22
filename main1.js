c03ea7970d07573d5dd036b8fadbfcc094fc1c8b
ehtesham <ehteshambahoo9023@gmail.com>
2023-03-22 22:57:04 +0400

    all files are commited

---
+++ main1.js
@@ -0,0 +1,92 @@
+let mySub =[
+
+    {
+        "id": 1001,
+        "title": "Integration crash course",
+        "location":"Main campus",
+        "price": 150,
+        "image": "img/math.jpg",
+        "availableInventory": 12
+       
+      }, 
+      {
+        "id": 1002,
+        "title": "English literature class",
+        "location":"Chelsea",
+        "price": 150,
+        "image": "img/english.jpg",
+        "availableInventory": 11       
+      }, 
+      {
+        "id": 1003,
+        "title": "Kinematics crash course",
+        "location":"Oxford",
+        "price": 80,
+        "image": "img/kine.png",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 1004,
+        "title": "Playing guitar basics",
+        "location":"New York",
+        "price": 100,
+        "image": "img/guitar.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 1005,
+        "title": "Learn to create a Web App",
+        "location":"Dubai",
+        "price": 150,
+        "image": "img/web.jpg",
+        "availableInventory": 15
+       
+      }, 
+      {
+        "id": 1006,
+        "title": "UI/UX basics",
+        "location":"Sharjah",
+        "price": 90,
+        "image": "img/ui.png",
+        "availableInventory": 10
+       
+      }, 
+      {
+        "id": 1007,
+        "title": "Differentiation Crash course",
+        "location":"Dubai",
+        "price": 90,
+        "image": "img/math.jpg",
+        "availableInventory": 6
+       
+      }, 
+      {
+        "id": 1008,
+        "title": "English",
+        "location":"Doha",
+        "price": 85,
+        "image": "img/english.jpg",
+        "availableInventory": 15
+       
+      }, 
+      {
+        "id": 1009,
+        "title": "Web App",
+        "location":"London",
+        "price": 100,
+        "image": "img/web.jpg",
+        "availableInventory": 5
+       
+      }, 
+      {
+        "id": 1010,
+        "title": "Starting your own business",
+        "location":"New York",
+        "price": 100,
+        "image": "img/bus.jpg",
+        "availableInventory": 10
+       
+      }, 
+]
